2b6fb0c0972deb851c3a986ad5d359c6f789fc12
s-banerjee94 <shondha.tara94@gmail.com>
2025-12-22 15:30:31 +0530

    feat: add linting and formatting scripts, update dependencies, and configure PrimeNG theme
    - Added linting and formatting scripts to package.json.
    - Updated dependencies including Angular, PrimeNG, TailwindCSS, and ESLint.
    - Configured PrimeNG theme in app.config.ts.
    - Improved HTML structure and formatting in app.html.
    - Updated app component and styles to integrate TailwindCSS and PrimeUI.
    - Added ESLint configuration for TypeScript.
    - Cleaned up TypeScript configuration files.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'be',
+          darkModeSelector: 'be-dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng',
+          },
+        },
+      },
+    }),
+  ],
 };
